9853bf33576d862f3270fe0cec8b1577d8f3b846
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-04-07 18:44:08 -0400

    Add counter button to test reactivity

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,9 +1,15 @@
 <script>
     let text = 'Frontend Developer self-taught';
+    let count = 0;
+
+    function handleClick(){
+        count += 1;
+    }
 </script>
 
 <div class="about">
     <p>{text}</p>
+    <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
 </div>
 
 <style>
