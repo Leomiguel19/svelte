edf110245e9117868d4bef7b42de8df2fe15040a
Leonardo Guilarte <leomiguel1907@gmail.com>
2023-04-07 22:16:00 -0400

    Lifecycle

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,7 @@
     import About from "./components/About.svelte";
     import Input from "./components/Input.svelte";
     import Person from "./components/Person.svelte";
+  import Photos from "./components/Photos.svelte";
     import Skills from "./components/Skills.svelte";
     import Text from "./components/Text.svelte";
 
@@ -24,6 +25,7 @@
     <Skills/>
     <Input />
     <img src={svelteLogo} alt="Svelte">
+    <Photos/>
 </main>
 
 <style>
